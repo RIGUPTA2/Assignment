fc3c35757b9383e4a687ccd4846f8d28a210acce
Rishi Gupta <rishi.cse@mietjammu.in>
2021-04-04 22:47:13 +0530

    Kafka setup bat scripts and library for kafka producer/consumer program

---
+++ 4X_ListTopics.bat
@@ -0,0 +1,4 @@
+Rem 1. Start Zookeeper after setting log.dirs in server.properties and dataDir in zookeeper.properties
+D:\
+cd D:\ProgramFiles\kafka
+.\bin\windows\kafka-topics.bat --list --zookeeper localhost:2181
--- SetUpEnvironment.bat
+++
@@ -1,4 +0,0 @@
-Rem 1. Store Json string in Redis
-javac -d classes\ -cp .;lib\redis\jedis-3.5.2.jar src\RedisSetString.java
-java -cp classes;lib\redis\jedis-3.5.2.jar RedisSetString
-
