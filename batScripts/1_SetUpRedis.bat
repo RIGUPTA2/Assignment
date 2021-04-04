54c717b21c274f3941358ab45cc69bd5b68cfc7b
Rishi Gupta <rishi.cse@mietjammu.in>
2021-04-04 22:53:12 +0530

    organizing bat scripts

--- batScripts/1_SetUpRedis.bat
+++ batScripts/1_SetUpRedis.bat
@@ -1,5 +1,5 @@
 Rem 0. Redis server is already installed using Windows MSI file and is running on system start and could be stopped/started from services.msc 
 
 Rem 1. Store JSON string in Redis
-javac -d classes\ -cp .;lib\redis\jedis-3.5.2.jar src\RedisSetString.java
-java -cp classes;lib\redis\jedis-3.5.2.jar RedisSetString
+javac -d ..\classes\ -cp .;..\lib\redis\jedis-3.5.2.jar ..\src\RedisSetString.java
+java -cp ..\classes;..\lib\redis\jedis-3.5.2.jar RedisSetString
