3325b57a4ee087830ac570e7548f51486b526b7c
Rishi Gupta <rishi.cse@mietjammu.in>
2021-04-05 11:56:29 +0530

    Working Producer and dependencies

--- batScripts/7_ProducerProgram.bat
+++ batScripts/7_ProducerProgram.bat
@@ -1,8 +1,8 @@
 Rem 0. Redis server is already installed using Windows MSI file and is running on system start and could be stopped/started from services.msc 
 
 Rem 1. Store JSON string in Redis
-javac -d ..\classes\ -cp .;..\lib\kafka\kafka-clients-2.7.0.jar;..\lib\kafka\slf4j-api-1.7.25.jar;..\lib\kafka\lz4-java-1.7.1.jar;..\lib\kafka\snappy-java-1.1.7.7.jar;..\lib\kafka\zstd-jni-1.4.5-6.jar;..\lib\kafka\slf4j-simple-1.7.25.jar ..\src\kafkaClients\ProducerProgram.java
+javac -d ..\classes\ -cp .;..\lib\kafka\kafka-clients-2.7.0.jar;..\lib\kafka\slf4j-api-1.7.30.jar;..\lib\kafka\lz4-java-1.7.1.jar;..\lib\kafka\snappy-java-1.1.7.7.jar;..\lib\kafka\zstd-jni-1.4.5-6.jar;..\lib\kafka\slf4j-simple-1.7.30.jar ..\src\kafkaClients\ProducerProgram.java
 
 
-
-java -cp ..\classes;..\lib\kafka\kafka-clients-2.7.0.jar;..\lib\kafka\slf4j-api-1.7.25.jar;..\lib\kafka\lz4-java-1.7.1.jar;..\lib\kafka\snappy-java-1.1.7.7.jar;..\lib\kafka\zstd-jni-1.4.5-6.jar;..\lib\kafka\slf4j-simple-1.7.25.jar ProducerProgram
+Rem 2. Publish message on kafka
+java -cp ..\classes;..\lib\kafka\kafka-clients-2.7.0.jar;..\lib\kafka\slf4j-api-1.7.30.jar;..\lib\kafka\lz4-java-1.7.1.jar;..\lib\kafka\snappy-java-1.1.7.7.jar;..\lib\kafka\zstd-jni-1.4.5-6.jar;..\lib\kafka\slf4j-simple-1.7.30.jar ProducerProgram
--- src/kafkaClients/ProducerProgram.java
+++ src/kafkaClients/ProducerProgram.java
@@ -8,16 +8,16 @@ public class ProducerProgram {
       //System.out.println("Input from data source"+JSON_DataFeedFromEdgeNode);
 
       Properties prop=new Properties();
-      prop.put(k: "bootstrap.servers", v: "localhost:9092");
-      prop.put(k: "key.serializer", v: "org.apache.kafka.common.serialization.StringSerializer");
-      prop.put(k: "value.serializer",v: "org.apache.kafka.common.serialization.StringSerializer");
+      prop.put("bootstrap.servers","localhost:9092");
+      prop.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+      prop.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
 
-      ProducerRecord producerRecord=new ProducerRecord(topic: "SourceTopic", key: "InputData", value: JSON_DataFeedFromEdgeNode);
+      ProducerRecord producerRecord=new ProducerRecord("SourceTopic","InputData", JSON_DataFeedFromEdgeNode);
 
       KafkaProducer kafkaProducer = new KafkaProducer(prop);
       kafkaProducer.send(producerRecord);
       kafkaProducer.close();
 
-      System.out.println("Stored string in redis: ");
-   } 
+      System.out.println("\nJSON Data feed from Edge node is submitted in Source Topic!");
+   }
 }
