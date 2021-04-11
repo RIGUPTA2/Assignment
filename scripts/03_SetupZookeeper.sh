18750c7a5a95a4658c03c6edf79ee369c9e4f875
Ubuntu <ubuntu@ip-172-31-6-101.ap-south-1.compute.internal>
2021-04-11 14:17:25 +0000

    Added end of file in zookeeper start script

--- scripts/03_SetupZookeeper.sh
+++ scripts/03_SetupZookeeper.sh
@@ -1,3 +1,4 @@
 # 1. Start Zookeeper after setting log.dirs in server.properties and dataDir in zookeeper.properties
 cd /home/ubuntu/kafka
 ./bin/zookeeper-server-start.sh ./config/zookeeper.properties
+
