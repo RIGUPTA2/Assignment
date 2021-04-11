266017b4d4895ca6d259f713ae13575bf401358e
Ubuntu <ubuntu@ip-172-31-6-101.ap-south-1.compute.internal>
2021-04-11 14:39:39 +0000

    Created topics with 2, 5, 10 and 20 partitions

--- scripts/05_CreateTopics.sh
+++ scripts/05_CreateTopics.sh
@@ -1,7 +1,10 @@
 #Create topics
 cd /home/ubuntu/kafka
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic SourceTopic
+./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic SourceTopic2
+./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 5 --topic SourceTopic5
 
-./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic EnrichedTopic
+./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 10 --topic SourceTopic10
+./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 20 --topic SourceTopic20
+#./bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic EnrichedTopic
 
 cd ../Assignment/scripts
