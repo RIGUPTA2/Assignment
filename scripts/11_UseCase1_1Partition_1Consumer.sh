3bab7e98c6895cbecc94948c3c66a84d339cebc6
Ubuntu <ubuntu@ip-172-31-6-101.ap-south-1.compute.internal>
2021-04-11 15:12:49 +0000

    Single script for all use cases

--- scripts/11_UseCase1_1Partition_1Consumer.sh
+++ scripts/11_UseCase1_1Partition_1Consumer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-. 09_AggregatorStream.sh 1partition_1consumerOnly 100000 SourceTopic &
-. 10_ProducerProgram.sh 1partition_1consumerOnly 100000 SourceTopic &
+#. 09_AggregatorStream.sh 1partition_1consumer_log1 100000 SourceTopic &
+#. 10_ProducerProgram.sh 1partition_1consumerOnly 100000 SourceTopic &
 
--- scripts/12_UseCase2_2Partition_2Consumer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. 09_AggregatorStream.sh 2partition_2consumerOnly 100000 SourceTopic2 &
-. 10_ProducerProgram.sh 2partition_2consumerOnly 100000 SourceTopic2 &
-
