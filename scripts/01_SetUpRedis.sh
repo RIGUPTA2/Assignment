295eae46fe135e47ef1e9ee37803cd7ab7e2a5a8
Ubuntu <ubuntu@ip-172-31-6-101.ap-south-1.compute.internal>
2021-04-11 14:08:22 +0000

    Updated Redis Set string and added classes folder

--- scripts/01_SetUpRedis.sh
+++ scripts/01_SetUpRedis.sh
@@ -1,4 +1,4 @@
 #Author: Rishi Gupta
 #Store JSON string in Redis
 javac -d ../classes/ -cp .:../lib/redis/jedis-3.5.2.jar ../src/redisClients/RedisSetString.java
-java -cp ../classes:../lib/redis/jedis-3.5.2.jar RedisSetString
+java -cp ../classes:../lib/redis/jedis-3.5.2.jar RedisSetString $1
